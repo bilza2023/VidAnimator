eb41c3fcacc86700569a2edb135236c24d167aa1
unknown <bilza32611@gmail.com>
2023-09-09 04:55:45 +0500

    next Text component -- 6 comps added

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ import VidAnimator from './lib/vidAnimator/VidAnimator';
 
 const vidAnimator = new VidAnimator();
 const scene01 = vidAnimator.addScene();
-const fillRect = scene01.addRectFill(300,200,50,50,'red');
 
-fillRect.setGraphicsAnchorX(2);
-fillRect.setGraphicsAnchorY(0);
+// const fillRect1 = scene01.addRectFill(0,0,50,50,'red');
+// const fillRect2= scene01.addRect(1,1,50,50,'green');
+// fillRect2.lineStyle(5, 0x000000);
+// fillRect1.tint = 'blue';
+// fillRect1.alpha = 0.1;
 
-scene01.addToTicker(() => {
+// const line= scene01.addLine(0,0,100,100,'red',5);
+const grid = scene01.addGridFull('#adaf67');
+const circle = scene01.addCircle(30,30,20,'red',5);
+const circleFull = scene01.addCircleFull(60,30,20,'red',5);
+
+vidAnimator.addToTicker(() => {
     // fillRect.rotation += 0.01;
-    fillRect.width +=0.5;
-    fillRect.height +=0.5;
-})
+});
 
 console.log('scene01' ,scene01);
 
---
+++ notes/tint_lineStyle.md
@@ -0,0 +1,8 @@
+- once a graphics is created the lineStyle is baked in 
+BUT
+we can change color if we start with white color and later just change 
+"tint"
+ and not color
+
+ However we can change ALPHA after Graphics creation.
+ i can use 2 fill rects to create a bordered rectangle******
